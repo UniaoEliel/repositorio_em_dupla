c1fd66c28f4fdf053955567050c479f779d3fb26
Elias Santos Martins <ra247057@snake.lab.ic.unicamp.br>
2022-05-12 14:39:29 -0300

    codigo inicial sala

--- lab05/src/pt/c40task/l05wumpus/Sala.java
+++ lab05/src/pt/c40task/l05wumpus/Sala.java
@@ -1,30 +1,74 @@
 package pt.c40task.l05wumpus;
 
+import java.util.ArrayList;
+
 public class Sala {
-	private Componente componentes[];
+	private ArrayList<Componente> componentes;
+	private int numComponentes;
 	private boolean visivel;
 	
 	public Sala() {
-		
+		componentes = new ArrayList<Componente>(2);
+		numComponentes = 0;
+		visivel = false;
 	}
 	
 	
 	public boolean inserirComponente(Componente comp) {
-		// verifica se a insercao é valida, se for retorna true
+		String tipoNovo = comp.getTipoComponente();
+		boolean insercaoValida = true;
+
+		for (int i = 0; i < numComponentes; i++) {
+			if (componentes.get(i).getTipoComponente() == tipoNovo) {
+				insercaoValida = false;
+				break;
+			}
+
+			if (tipoNovo == "wumpus" && componentes.get(i).getTipoComponente() == "ouro")  {
+				insercaoValida = false;
+				break;
+			}
+
+			if (tipoNovo == "ouro" && componentes.get(i).getTipoComponente() == "wumpus")  {
+				insercaoValida = false;
+				break;
+			}
+		}
+		
+		if (insercaoValida) {
+			componentes.add(comp);
+			numComponentes++;
+		}
+		
+		return insercaoValida;
 	}
 	
 	
 	public void removerComponente(String tipoComponente) {
-		// remove se o componente estiver, se nao nao faz nada
-	}
-	
-	
-	public String getComponentes() {
-		// retorna a string dos componentes da sala na ordem q o roteiro pede
-	}
-	
-	
-	public void tornarVisivel() {
+		String[] tipoComponentes = getTipoComponentes();
 		
+		for (int i = 0; i < componentes.size(); i++)
+			if (tipoComponentes[i] == tipoComponente)
+				componentes.remove(i);
+	}
+	
+	
+	public String[] getTipoComponentes() {
+		String[] tipoComponentes = new String[numComponentes];
+
+		for (int i = 0; i < numComponentes; i++)
+			tipoComponentes[i] = componentes.get(i).getTipoComponente();
+		
+		return tipoComponentes;
+	}
+	
+	
+	public boolean getVisivel() {
+		return visivel;
+	}
+
+	
+	public void setVisivel(boolean visivel) {
+		this.visivel = visivel;
 	}
 }
