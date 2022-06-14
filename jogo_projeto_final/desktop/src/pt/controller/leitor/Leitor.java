fef3199e52c0d4f2f622d714d6449c6aa13bc84a
Gabriel Sanders Pereira Sobral <ra247118@pessoa.lab.ic.unicamp.br>
2022-06-14 11:09:07 -0300

    inicia controller

--- jogo_projeto_final/desktop/src/pt/controller/leitor/Leitor.java
+++ jogo_projeto_final/desktop/src/pt/controller/leitor/Leitor.java
@@ -1,5 +1,36 @@
 package pt.controller.leitor;
 
+import pt.model.caverna.Caverna;
+import pt.model.caverna.Celula;
+
 public class Leitor implements ILeitor {
 
+	
+	
+	public int[] getTamanhoCaverna(Caverna caverna) {
+		int[] tamanho = new int [2];
+		tamanho[0] = caverna.getTamX();
+		tamanho[1] = caverna.getTamY();
+		return tamanho;	
+	}
+	
+	
+	public String[][] getAtoresCaverna(Caverna caverna, Celula celula){
+		
+	}
+	
+	
+	public String[][] getTexturas(){
+		
+	}
+	
+	
+	public String[][] getLayers(){
+		
+	}
+	
+	
+	public String[] getNomeArquivosTexturas() {
+		
+	}
 }
