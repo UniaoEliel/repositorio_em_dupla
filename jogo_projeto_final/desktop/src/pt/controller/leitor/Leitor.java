b84bd3a6bb653507754b8389c7fef95bff3b1cba
Gabriel Sanders Pereira Sobral <ra247118@legiao.lab.ic.unicamp.br>
2022-06-21 14:03:15 -0300

    atualiza leitor

--- jogo_projeto_final/desktop/src/pt/controller/leitor/Leitor.java
+++ jogo_projeto_final/desktop/src/pt/controller/leitor/Leitor.java
@@ -1,6 +1,9 @@
 package pt.controller.leitor;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 import pt.model.caverna.Caverna;
 import pt.model.caverna.Celula;
@@ -14,29 +17,70 @@ public class Leitor implements ILeitor {
 	
 	public int[] getTamanhoCaverna() {
 		 try{
-			  // Open the file that is the first 
-			  // command line parameter
 			  FileInputStream fstream = new FileInputStream(System.getProperty("user.dir") + "/src/pt/controller/cave.txt");
-			  // Get the object of DataInputStream
+			  
 			  DataInputStream in = new DataInputStream(fstream);
 			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			  String strLine;
-			  //Read File Line By Line
-			  strLine = br.readLine();
-			  // Print the content on the console
-			  System.out.println (strLine);
+			  String linhaTamanho;
 			  
-			  //Close the input stream
-			  in.close();
+			  //Le a primeira linha do arquivo cave.txt
+			  linhaTamanho = br.readLine();
+			 
+		 
+		 	//Separa o texto em Strings a partir do padrão
+	        String patternString = ";";
+	        Pattern pattern = Pattern.compile(patternString);	        
+	        String[] split = pattern.split(linhaTamanho);
+	        
+	        int x = Integer.parseInt(split[1]);
+	        int y =Integer.parseInt(split[2]);
+	        
+	        int [] tamanho =  {x,y};
+	        return tamanho;
+	        
 			    }catch (Exception e){//Catch exception if any
 			  System.err.println("Error: " + e.getMessage());
-			  }	
+			 }	
+	    
 		int[] tamanhos = {100, 100};
 		return tamanhos;
 	}
 	
 	
 	public String[][] getAtoresCaverna(){
+		try{
+			  FileInputStream fstream = new FileInputStream(System.getProperty("user.dir") + "/src/pt/controller/cave.txt");
+			  
+			  DataInputStream in = new DataInputStream(fstream);
+			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			  String linhaAtor;
+	        //fazer arraylist
+	
+			ArrayList<String[]> matrix = new ArrayList<String[]>();
+			
+	        while ((linhaAtor = br.readLine()) != null)   {
+	        	String patternString = ";";
+		        Pattern pattern = Pattern.compile(patternString);	        
+		        String[] split = pattern.split(linhaAtor);
+		        
+		        if (split.length == 4) {
+		        ArrayList<String> row = new ArrayList<String>(Arrays.asList(split[0],split[1],split[2],split[3]));
+		        matrix.add(split);
+		        }
+	        }
+	        
+	        String [][] atoresCaverna = new String[matrix.size()][4];
+	        for (int i=0;i < matrix.size(); i++) {
+	        	atoresCaverna[i] = matrix.get(i); 
+	        }
+	        
+	        return atoresCaverna;
+	        
+	        	
+	        
+			    }catch (Exception e){//Catch exception if any
+			  System.err.println("Error: " + e.getMessage());
+			 }
 		String[][] atores = {
 				{"heroi", "w", "5", "5"},
 				{"parede", "s", "6", "6"},
